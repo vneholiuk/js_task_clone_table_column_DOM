772ac275d189a8e6dcd49658b274b728cf73ebfc
Vlados <vnegoluk@gmail.com>
2024-08-03 16:11:42 +0200

    make build

---
+++ index.81a81dad.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("table tr").forEach(function(e){var t=e.querySelectorAll("th, td"),l=t[1].cloneNode(!0);e.insertBefore(l,t[t.length-1])});
+//# sourceMappingURL=index.81a81dad.js.map
